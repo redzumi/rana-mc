d14e1ef88d21515558844c92b364b3231b4e95bf
Evgenii Bykovskikh <redzumi.tao@gmail.com>
2025-04-06 13:49:58 +0300

    Add mods reading

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-console.log("Hello world");
+
+import * as path from 'path';
+
+import { readFilesInMinecraftFolder } from './utils';
+
+// TODO: Replace by choosing from ui?
+const multiMCVersion = '1.20.4';
+const multiMCPath = path.join('C:', 'Games', 'MultiMC', 'instances', multiMCVersion, '.minecraft');
+
+const minecraftPath = path.join(process.env.APPDATA || '', '.minecraft');
+
+const usingMultiMC = true;
+const modsPath = path.join(usingMultiMC ? multiMCPath : minecraftPath, 'mods');
+
+const main = async () => {
+  const files = await readFilesInMinecraftFolder(modsPath);
+  console.log(files);
+};
+
+main();
---
+++ src/utils.ts
@@ -0,0 +1,20 @@
+import * as fs from 'fs';
+import * as path from 'path';
+
+export const readFilesInMinecraftFolder = async (folderPath: string): Promise<{ path: string, filename: string }[]> => {
+    try {
+        const files = await fs.promises.readdir(folderPath);
+
+        const getFileStats = async (file: string) => {
+            const filePath = path.join(folderPath, file);
+            const stats = await fs.promises.stat(filePath);
+
+            return stats.isFile() ? { path: filePath, filename: file } : null;
+        };
+
+        const fileStats = await Promise.all(files.map(getFileStats));
+        return fileStats.filter((file) => file !== null) as { path: string, filename: string }[];
+    } catch (err) {
+        throw new Error(err.message);
+    }
+};
