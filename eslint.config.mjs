ea96f6c948f0edea895eb53bf51678500a22a413
Evgenii Bykovskikh <redzumi.tao@gmail.com>
2025-04-06 13:34:52 +0300

    Initial commit

---
+++ eslint.config.mjs
@@ -0,0 +1,61 @@
+import tsParser from "@typescript-eslint/parser";
+import tsPlugin from "@typescript-eslint/eslint-plugin";
+import importPlugin from "eslint-plugin-import";
+import sortPlugin from "eslint-plugin-simple-import-sort";
+import prettierConfig from "eslint-config-prettier";
+import globals from "globals";
+
+import { defineConfig } from "eslint/config";
+import { includeIgnoreFile } from "@eslint/compat";
+import { fileURLToPath } from "node:url";
+
+const gitignorePath = fileURLToPath(new URL(".gitignore", import.meta.url));
+
+export default defineConfig([
+  includeIgnoreFile(gitignorePath),
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+      },
+      globals: {
+        ...globals.node,
+        ...globals.commonjs,
+      },
+    },
+    plugins: {
+      "@typescript-eslint": tsPlugin,
+      import: importPlugin,
+      "simple-import-sort": sortPlugin,
+    },
+    rules: {
+      ...tsPlugin.configs.recommended.rules,
+
+      "import/first": "error",
+      "import/newline-after-import": "error",
+      "import/no-duplicates": "error",
+
+      "simple-import-sort/imports": "error",
+      "simple-import-sort/exports": "error",
+    },
+    settings: {
+      "import/resolver": {
+        typescript: {
+          alwaysTryTypes: true,
+        },
+        node: {
+          extensions: [".ts", ".tsx", ".js", ".jsx"],
+        },
+      },
+      "import/internal-regex": "^~/",
+    },
+  },
+  {
+    rules: {
+      ...prettierConfig.rules,
+    },
+  },
+]);
